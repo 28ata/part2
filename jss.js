41a76bf51dbe7f8b83c5d4647c70cde9d72aa668
28ata <80188846+28ata@users.noreply.github.com>
2021-08-29 13:53:00 -0700

    Add files via upload

---
+++ jss.js
@@ -0,0 +1,8 @@
+p1=localStorage.getItem("player1name");
+p2=localStorage.getItem("player2name");
+player1_score=0;
+player2_score=0;
+document.getElementById("player1name").innerHTML = player1_name + ":";
+document.getElementById("player2name").innerHTML = player2_name + ":";
+document.getElementById("p1score").innerHTML = player1_score;
+document.getElementById("p1score").innerHTML = player2_score;
